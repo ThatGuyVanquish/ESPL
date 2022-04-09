515bf65b376d8928e4488052ce041ea2376942cf
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-04-09 12:44:34 +0000

    Lab 3 Task 1a+b

--- Lab 3/T1/lab3.c
+++ Lab 3/T1/lab3.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>	
 #include <stdlib.h>
 
-void PrintHex(char *buffer, size_t length)
+void PrintHex(unsigned char *buffer, size_t length)
 {
 	for(int i = 0; i < length; i++)
-		printf("%x ", buffer[i]);
+		printf("%02X ", buffer[i]);
+	printf("\n");
 }
 
 typedef struct virus {
@@ -13,32 +14,128 @@ typedef struct virus {
     char virusName[16];
 } virus;
 
+typedef struct link link;
+ 
+struct link {
+    link *nextVirus;
+    virus *vir;
+};
+
 void readVirus(virus* vir, FILE* input) {
-	//fread(vir->virusName, 16, 1, input);
-	//fread(vir->sig, vir->SigSize, 1, input);
-	char* size = malloc(2);
-	printf("previous size is %x\n", size);
+	char size[2];
 	fread(size, 2, 1, input);
-	printf("hex size is %x\n", size);
-	printf("%d %s\n", vir->SigSize, size);
-
-
+	vir->SigSize = size[0] | size[1] << 8;
+	vir->sig = malloc(vir->SigSize);
+	fread(vir->sig, 1, vir->SigSize, input);
+	fread(vir->virusName, 1, 16, input);
 }
 
 void printVirus(virus* vir, FILE* output)
 {
+	if (vir->SigSize == 0)
+		return;
 	fprintf(output, "Virus Name: %s\n", vir->virusName);
 	fprintf(output, "Virus Size: %d\n", vir->SigSize);
 	fprintf(output, "Virus Signature:\n");
-	PrintHex(vir->sig, sizeof(vir->sig));
+	PrintHex(vir->sig, vir->SigSize);
+	printf("\n");
+}
+
+link* list_print(link *virus_list, FILE* output) {
+	if (virus_list == NULL)
+		return NULL;
+	printVirus(virus_list->vir, output);
+	list_print(virus_list->nextVirus, output);
+	return virus_list;
+}
+
+link* list_append(link* virus_list, link* to_add) {
+	// If we need to add to head of the list:
+	to_add->nextVirus = virus_list;
+	return to_add;
+	// If we need to add to the tail of the list:
+	if (virus_list == NULL) {
+		virus_list = to_add;
+		return virus_list;
+	}
+	list_append(virus_list->nextVirus, to_add);
+	return virus_list;
+}
+
+void list_free(link *virus_list) {
+	if (virus_list == NULL)
+		return;
+	else {
+		list_free(virus_list->nextVirus);
+		free(virus_list);
+	}
+}
+
+typedef struct fun_desc {
+  char *name;
+  link* (*fun)(link*, FILE*);
+} fun_desc;
+
+link* quit(link *virus_list, FILE* output)
+{
+	if (virus_list != NULL)
+		list_free(virus_list);
+	exit(0);
+	return virus_list;
+}
+
+link *loadViruses(link *virus_list, FILE* output)
+{	
+	char fileName[100];
+	printf("Please enter a file name\n");
+	scanf("%[^\n]%*c", fileName);
+	printf("File name is %s\n", fileName);
+	if (sizeof(fileName) == 0)
+		return NULL;
+	FILE *fp = fopen(fileName, "r");
+	link *vlist;
+	if (fp == NULL) {
+		fprintf(stderr, "Can't open file\n");
+		exit(1);
+	}
+	while (!feof(fp)) {
+		virus *vir = malloc(sizeof(virus));
+		readVirus(vir, fp);
+		link *toInsert = malloc(sizeof(link));
+		toInsert->vir = vir;
+		vlist = list_append(vlist, toInsert);
+	}
+	fclose(fp);
+	return vlist;
 }
 
 int main(int argc, char **argv)
 {
-	FILE *fp = fopen(argv[1], "r");
-	if (fp == NULL) printf("BLYAT");
-	virus *vir = malloc(sizeof(virus));
-	readVirus(vir, fp);
-	printVirus(vir, stdout);
+	link *virus_list = NULL;
+	struct fun_desc menu[] = { 
+	{"Load signatures", loadViruses}, 
+	{"Print signatures", list_print},
+	{"Quit", quit},
+	{NULL, NULL}};
+	FILE *output = stdout;
+	do {
+		printf("Please choose a function:\n");
+		int len = sizeof(menu)/sizeof(struct fun_desc) - 1;
+		for(int i = 0; i < len; i++)
+			printf("%d) %s\n", i, menu[i].name);
+		int input = fgetc(stdin) - '0';
+		fgetc(stdin);
+		printf("Option: %d\n", input);
+		if (input >= 0 && input < len)
+			printf("Within bounds\n");
+		else
+		{
+			printf("Not within bounds\n");
+			list_free(virus_list);
+			exit(0);
+		}
+		virus_list = menu[input].fun(virus_list, output);
+	}
+	while(1);
 	return 0;
 }
