caab448f5cece186169caf7d8ced7b2c0821cb8d
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-05-21 19:26:20 +0300

    Started lab 6 part 0

---
+++ Lab 6/T0a/mypipe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    char c;
+    int p[2];
+
+    pipe(p);
+    int pid = fork();
+
+    if (pid > 0)
+    {
+        close(p[1]);
+        printf("Reading from parent\n");
+        while (read(p[0], &c, 1) != 0)
+            putchar(c);    
+        close(p[0]);
+        wait(NULL);
+    }
+    else 
+    {
+        close(p[0]);
+        write(p[1], "hello\n", sizeof("hello\n"));
+        close(p[1]);
+    }
+    return 0;
+}
